f008a7f9dc55306f92bb7b0cc1bcaa2177fff5c3
FischerDRT <andre.luiz_duarte@hotmail.com>
2025-06-12 00:27:54 +0000

    Desafio Novato

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,40 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+    {Printf("desafio Xadrez!\n");}
+}
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int main() {
+        {Printf("Desafio Novato!\n");}
+    // Movimento da Torre
+    printf("Movimento da Torre\n");
+    int t;
+    for (t = 0; t < 5; t++) {
+        printf("Direita\n");
+    }
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Movimento do Bispo
+    printf("Movimento do Bispo\n");
+    int b = 0;
+    while (b < 5) {
+        printf("Cima Direita\n");
+        b++;
+    }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("\n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    // Movimento da Rainha 
+    printf("Movimento da Rainha\n");
+    int r = 0;
+    do {
+        printf("Esquerda\n");
+        r++;
+    } while (r < 8);
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+     printf("\n");
 
     return 0;
 }
+
